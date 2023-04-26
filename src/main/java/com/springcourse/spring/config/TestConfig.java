c1c66bf54208b38f9b099298cbdbccec59fcd6bc
Erick Yuta <erick_miyagi34231@hotmail.com>
2023-04-26 11:03:08 -0300

    Component Register

--- src/main/java/com/springcourse/spring/config/TestConfig.java
+++ src/main/java/com/springcourse/spring/config/TestConfig.java
@@ -14,14 +14,14 @@ import com.springcourse.spring.repositories.UserRepository;
 @Profile("test")
 public class TestConfig implements CommandLineRunner{ //database seeding
 	@Autowired //realiza automaticamente 
-	private UserRepository userRepository;
+	private UserRepository userRepository; //Injection Dependency
 
 @Override
 public void run(String... args) throws Exception {
 	User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 	User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 	
-	userRepository.saveAll(Arrays.asList(u1,u2));
+	userRepository.saveAll(Arrays.asList(u1,u2)); //Save in database
 }
 	
 }
--- src/main/java/com/springcourse/spring/resources/UserResource.java
+++ src/main/java/com/springcourse/spring/resources/UserResource.java
@@ -1,19 +1,33 @@
 package com.springcourse.spring.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springcourse.spring.entities.User;
+import com.springcourse.spring.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource { //Recurso Web que vai ser controlado por um Rest e ligar com o User
 	
+	@Autowired
+	private UserService service; //injection dependency
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L,"Maria", "maria@hotmail.com", "113984984", "12345"); //Create a new User in json
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
+	@GetMapping(value = "/{id}") //requisição para aceitar um id na URL
+	public ResponseEntity<User> findById(@PathVariable Long id){ //PathVariable = O spring vai aceitar isso com id, só por conta do Path
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 }
