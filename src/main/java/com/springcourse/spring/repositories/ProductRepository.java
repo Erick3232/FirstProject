59c7571156cda77e1c227e33294f769fe78d91e9
Erick Yuta <erick_miyagi34231@hotmail.com>
2023-05-01 10:14:56 -0300

    add product entity

---
+++ src/main/java/com/springcourse/spring/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.springcourse.spring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springcourse.spring.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+	
+}
